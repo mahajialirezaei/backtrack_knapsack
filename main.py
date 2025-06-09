2cabb7cc3ab857a90de899d717cf07e25c4ed8eb
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-09 20:38:50 +0330

    add new func "recursive_func"

---
+++ main.py
@@ -0,0 +1,11 @@
+def recursive_func(x:list, p:list, x_opt:list, p_opt, l):
+    if l == len(x) + 1:
+        p_cur = cal_profit(x, p)
+        if p_cur > p_opt:
+            p_opt =  p_cur
+            x_opt = x
+    else:
+        x[l] = 1
+        recursive_func(x, p, x_opt, p_opt, l + 1)
+        x[l] = 0
+        recursive_func(x, p, x_opt, p_opt, l + 1)
