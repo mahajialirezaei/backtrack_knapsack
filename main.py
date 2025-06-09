e814e983a1e1c13cfe41309649328715a8679e8c
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-09 20:41:17 +0330

    define new func"cal_profit"

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+def cal_profit(x:list, p:list):
+    profit = sum(s * s1 for s, s1 in (x,p))
+    return profit
 def recursive_func(x:list, p:list, x_opt:list, p_opt, l):
     if l == len(x) + 1:
         p_cur = cal_profit(x, p)
