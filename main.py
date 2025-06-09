dece18bb92e9ed0eb02a8fbe9927f8a12499402e
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-09 20:45:34 +0330

    change cal_profit to cal_product_ls and change recursive_func

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-def cal_profit(x:list, p:list):
+def cal_product_ls(x:list, p:list):
     profit = sum(s * s1 for s, s1 in (x,p))
     return profit
-def recursive_func(x:list, p:list, x_opt:list, p_opt, l):
+def recursive_func(x:list, p:list, x_opt:list, w:list ,p_opt, l, b):
     if l == len(x) + 1:
-        p_cur = cal_profit(x, p)
-        if p_cur > p_opt:
-            p_opt =  p_cur
-            x_opt = x
+        if cal_product_ls(x, w) <= b:
+            p_cur = cal_product_ls(x, p)
+            if p_cur > p_opt:
+                p_opt =  p_cur
+                x_opt = x
     else:
         x[l] = 1
-        recursive_func(x, p, x_opt, p_opt, l + 1)
+        recursive_func(x, p, x_opt, w,p_opt, l + 1, b)
         x[l] = 0
-        recursive_func(x, p, x_opt, p_opt, l + 1)
+        recursive_func(x, p, x_opt, w, p_opt, l + 1, b)
+
+x = [0, 0, 0]
+p = [1, 2, 3]
+w = [1, 2, 999]
+b = 1000
